af501b2e99177b3cf4eb8d43786d0056507a72f9
Hinekureta <herrbach.kevin@gmail.com>
2017-09-19 15:15:19 +0200

    [FRONT] Displays home page using react

--- web/js/react/index.jsx
+++ web/js/react/index.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+class PostsList extends React.Component {
+    render() {
+        var rows = [];
+        var postZones = document.getElementsByClassName('post-zone');
+        for (var i = 0; i < postZones.length; ++i) {
+            var post = postZones[i];
+            rows.push(
+                <div key={i}>
+                    <a href={window.location.href + "/posts/" + post.getAttribute('data-id')}>
+                        {post.getAttribute('data-date')}
+                        <h3>{post.getAttribute('data-title')}</h3>
+                        {post.getAttribute('data-content')}
+                    </a>
+                </div>
+            );
+        }
+        {/*postZones.forEach(function (post) {*/}
+        //    rows.push(
+        //        <div>
+        //        <a href={window.location.href + "/posts/" + post.getAttribute('data-id')}>
+        //        {post.getAttribute('data-date')}
+        //        <h3>{post.getAttribute('data-title')}</h3>
+        //        {post.getAttribute('data-content')}
+        //        </a>
+        //        </div>
+        //    );
+        // });
+        return (
+            <div>{rows}</div>
+        );
+    }
+}
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <PostsList />,
     document.getElementById('root')
 );
