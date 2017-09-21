b5499614fc147e0a594bf2d103a0b95e9dfdbdab
Hinekureta <herrbach.kevin@gmail.com>
2017-09-21 19:59:36 +0200

    Removes comment

--- web/js/react/index.jsx
+++ web/js/react/index.jsx
@@ -2,28 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PostList from './components/PostList'
 
-// class PostsList extends React.Component {
-//     render() {
-//         var rows = [];
-//         var postZones = document.getElementsByClassName('post-zone');
-//         for (var i = 0; i < postZones.length; ++i) {
-//             var post = postZones[i];
-//             rows.push(
-//                 <div key={i}>
-//                     <a href={window.location.href + "/posts/" + post.getAttribute('data-id')}>
-//                         {post.getAttribute('data-date')}
-//                         <h3>{post.getAttribute('data-title')}</h3>
-//                         {post.getAttribute('data-content')}
-//                     </a>
-//                 </div>
-//             );
-//         }
-//         return (
-//             <div>{rows}</div>
-//         );
-//     }
-// }
-
 ReactDOM.render(
     <PostList posts={document.getElementsByClassName('post-zone')}/>,
     document.getElementById('root')
