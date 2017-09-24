2455673db4877d6005eb931d8f0cad1d883196ab
Hinekureta <herrbach.kevin@gmail.com>
2017-09-24 21:10:55 +0200

    Removes react components

--- web/js/react/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PostList from './components/PostList'
-
-ReactDOM.render(
-    <PostList posts={document.getElementsByClassName('post-zone')}/>,
-    document.getElementById('root')
-);
