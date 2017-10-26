476de2cef1a8409f07b8e1009b5bdacffe8ab79f
Hinekureta <herrbach.kevin@gmail.com>
2017-10-26 22:37:52 +0200

    Add coindata entity

---
+++ src/AppBundle/Entity/CoinData.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+const BITCOIN_TYPE = 10;
+const ETHEREUM_TYPE = 20;
+const LITECOIN_TYPE = 30;
+
+/**
+ * CoinData
+ *
+ * @ORM\Table(name="coin_data")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CoinDataRepository")
+ */
+class CoinData
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="value", type="float")
+     */
+    private $value;
+
+    /**
+     * @ORM\Column(name="currency_type", type="integer")
+     */
+    private $type;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return CoinData
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set value
+     *
+     * @param float $value
+     *
+     * @return CoinData
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return float
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param mixed $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+}
+
