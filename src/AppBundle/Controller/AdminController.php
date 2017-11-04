c9d9e6c9eb2798914428a5e8b12829c200afed0e
Hinekureta <herrbach.kevin@gmail.com>
2017-11-04 15:34:14 +0100

    Adds every movies and series to new post page

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -48,8 +48,14 @@ class AdminController extends Controller
             return $this->redirectToRoute('homepage');
         }
 
+        $series = scandir('./series');
+        $movies = scandir('./movies');
+        $series = array_slice($series, 2);
+        $movies = array_slice($movies, 2);
         return $this->render(':admin:new-post.html.twig', [
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'movies' => $movies,
+            'series' => $series,
         ]);
     }
 }
